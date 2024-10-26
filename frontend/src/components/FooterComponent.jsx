8ca0d84b890d1867bdeeb7d10c4f921df342c375
pavlo <frontbastard@gmail.com>
2024-10-26 21:32:52 +0200

    [FE] Add footer component

---
+++ frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import {Typography} from '@mui/material';
+import {grey} from '@mui/material/colors';
+
+function FooterComponent() {
+  return (
+    <Typography align="center"
+                sx={{backgroundColor: grey['800'], py: 5, mt: 5}}>
+      Copyright &copy; all rights reserved.
+    </Typography>
+  )
+}
+
+export default FooterComponent
